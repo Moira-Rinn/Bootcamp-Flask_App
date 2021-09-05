199ca32bc384b8eeb31b2f24c958625a32adeffa
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-09-05 01:25:25 -0400

    Initial commit, setup, flawed code...

---
+++ controllers/burgers.py
@@ -0,0 +1,76 @@
+from flask import render_template, redirect, request, session, flash
+from Flask_App import app
+from Flask_App.models.burger import Burger
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# gets all the burgers and returns them in a list of burger objects .
+
+
+@app.route('/create/burger', methods=['POST'])
+def create_burger():
+    data = {
+        "name": request.form['name'],
+        "bun": request.form['bun'],
+        "meat": request.form['meat'],
+        "calories": request.form['calories']
+    }
+    Burger.save(data)
+    return redirect('/burgers')
+
+
+@app.route('/burgers')
+def burgers():
+    query = "SELECT * FROM burgers;"
+    burgers = connectToMySQL('burgers').query_db(query)
+    return render_template("results.html", all_burgers=burgers)
+
+
+@app.route('/show/<int:burger_id>')
+def detail_page(burger_id):
+    query = "SELECT * FROM burgers WHERE burgers.id = %(id)s;"
+    data = {
+        'id': burger_id
+    }
+    results = connectToMySQL('burgers').query_db(query, data)
+
+    return render_template("details_page.html", burger=results[0])
+
+
+@app.route('/edit_page/<int:burger_id>')
+def edit_page(burger_id):
+    query = "SELECT * FROM burgers WHERE id = %(id)s;"
+    data = {
+        'id': burger_id
+    }
+    burger = connectToMySQL('burgers').query_db(query, data)
+    print(burger)
+    return render_template("edit_page.html", burger=burger[0])
+
+
+@app.route('/update/<int:burger_id>', methods=['POST'])
+def update(burger_id):
+    query = "UPDATE burgers SET name=%(name)s, bun=%(bun)s, meat=%(meat)s, calories=%(calories)s,updated_at = NOW() WHERE id = %(id)s;"
+    data = {
+        'id': burger_id,
+        "name": request.form['name'],
+        "bun": request.form['bun'],
+        "meat": request.form['meat'],
+        "calories": request.form['calories']
+    }
+    burger = connectToMySQL('burgers').query_db(query, data)
+    print(burger)
+    return redirect(f"/show/{burger_id}")
+
+
+@app.route('/delete/<int:burger_id>')
+def delete(burger_id):
+    query = "DELETE FROM burgers WHERE id = %(id)s;"
+    data = {
+        'id': burger_id,
+    }
+    connectToMySQL('burgers').query_db(query, data)
+    return redirect('/burgers')
---
+++ models/burger.py
@@ -0,0 +1,13 @@
+from Flask_App.config.mysql_connection import connectToMySQL
+# burger.py
+
+
+class Burger:
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.bun = data['bun']
+        self.meat = data['meat']
+        self.calories = data['calories']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
